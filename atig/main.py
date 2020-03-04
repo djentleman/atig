4be2ba02c7232819deb2b8aa1d84b3d53cdc288a
Todd Perry <up612136@myport.ac.uk>
2020-03-05 01:46:58 +0900

    now displays contents of migrations in subwindow on enter

--- atig/main.py
+++ atig/main.py
@@ -4,20 +4,26 @@ import re
 import curses
 from atig.migration import Migration, MigrationCollection
 
+
 class Atig():
     def __init__(self):
         self.valid_migration = re.compile(r'[0-9a-f]{12}\_.*\.py')
         self.migrations = None
         self.migration_collection = None
         self.debug_message = 'Welcome to ATIG 8^)'
+        self.window_open = False
+        self.display_window_contents = []
+        self.close_atig = False
 
         if self.is_in_alembic_directory() and self.alembic_is_installed():
             # first run ls, get all self.migrations and all hashes
             self.migrations = os.listdir('alembic/versions/')
-            self.migrations = map(lambda x: Migration('alembic/versions/' + x),
-                             filter(self.valid_migration.match, self.migrations)
-            )
-            self.migration_collection = MigrationCollection(list(self.migrations))
+            self.migrations = map(
+                lambda x: Migration('alembic/versions/' + x),
+                filter(self.valid_migration.match, self.migrations))
+            self.migration_collection = MigrationCollection(
+                list(self.migrations))
+            self.migrations = list(self.migration_collection.list_migrations())
 
     def init_screen(self, stdscr):
         self.stdscr = stdscr
@@ -29,7 +35,7 @@ class Atig():
         # Clear and refresh the screen for a blank canvas
         self.stdscr.clear()
         self.stdscr.refresh()
-        
+
         # Start colors in curses
         curses.start_color()
         curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
@@ -47,24 +53,41 @@ class Atig():
             self.cursor_x = self.cursor_x + 1
         elif self.k == curses.KEY_LEFT:
             self.cursor_x = self.cursor_x - 1
-        elif self.k == curses.KEY_ENTER:
-            self.display_window = curses.newwin(5, 5, 5, 5)
-            self.display_window.border()
+        elif self.k in [curses.KEY_ENTER, 10, 13]:
+            self.debug_message = ''
+            self.window_open = True
+            self.display_window = curses.newwin(0, self.width // 2, 0,
+                                                self.width // 2)
+            if self.cursor_y >= 1 and self.cursor_y <= len(self.migrations):
+                curr = self.migrations[self.cursor_y - 1]
+                self.debug_message = curr.hash
+                self.display_window_contents = curr.contents.split(
+                    '\n')[:self.height - 2]
+            else:
+                self.debug_message = 'no migration selected'
+        elif self.k == ord('q'):
+            if self.window_open:
+                self.window_open = False
+                self.display_window.erase()
+                self.display_window.refresh()
+                self.debug_message = ''
+                self.display_window_contents = []
+            else:
+                self.close_atig = True
         self.cursor_x = max(0, self.cursor_x)
-        self.cursor_x = min(self.width-1, self.cursor_x)
+        self.cursor_x = min(self.width - 1, self.cursor_x)
         self.cursor_y = max(0, self.cursor_y)
-        self.cursor_y = min(self.height-1, self.cursor_y)
+        self.cursor_y = min(self.height - 1, self.cursor_y)
 
     def render_status_bar(self):
         statusbarstr = f"Press 'q' to exit | {self.debug_message} | Pos: {self.cursor_x}, {self.cursor_y}"
 
         # Render status bar
         self.stdscr.attron(curses.color_pair(3))
-        self.stdscr.addstr(self.height-1, 0, statusbarstr)
-        self.stdscr.addstr(self.height-1, len(statusbarstr), " " * (self.width - len(statusbarstr) - 1))
+        self.stdscr.addstr(self.height - 1, 0, statusbarstr)
+        self.stdscr.addstr(self.height - 1, len(statusbarstr),
+                           " " * (self.width - len(statusbarstr) - 1))
         self.stdscr.attroff(curses.color_pair(3))
-
-
 
     def event_loop(self):
         # Initialization
@@ -72,9 +95,7 @@ class Atig():
         self.height, self.width = self.stdscr.getmaxyx()
         # Loop where k is the last character pressed
 
-
-        while (self.k != ord('q')):
-            self.process_input()
+        while (not self.close_atig):
             self.render_status_bar()
             if not self.alembic_is_installed():
                 self.render_alembic_install_error()
@@ -84,19 +105,25 @@ class Atig():
             else:
                 self.render_directory_error()
 
-
             self.stdscr.move(self.cursor_y, self.cursor_x)
             # Refresh the screen
             self.stdscr.refresh()
+            if self.window_open:
+                for i, row in enumerate(self.display_window_contents):
+                    self.display_window.addstr(i + 1, 1,
+                                               row[:(self.width // 2) - 2])
+                self.display_window.border()
+                self.display_window.refresh()
 
             # Wait for next input
             self.k = self.stdscr.getch()
-    
-
+            self.process_input()
+        curses.endwin()
 
     def fetch_migration_information(self):
-        title = "Alembic Migrations"[:self.width-1]
-        start_x_title = int((self.width // 2) - (len(title) // 2) - len(title) % 2)
+        title = "Alembic Migrations"[:self.width - 1]
+        start_x_title = int((self.width // 2) - (len(title) // 2) -
+                            len(title) % 2)
         # Turning on attributes for title
         self.stdscr.attron(curses.color_pair(2))
         self.stdscr.attron(curses.A_BOLD)
@@ -109,17 +136,20 @@ class Atig():
         self.stdscr.attroff(curses.A_BOLD)
 
         # finally we need to render them on the screen
-        for i, migration in enumerate(self.migration_collection.list_migrations()):
+        for i, migration in enumerate(
+                self.migration_collection.list_migrations()):
             self.stdscr.addstr(1 + i, 0, migration.date, curses.color_pair(0))
             if self.migration_collection.current in migration.name:
                 self.stdscr.attron(curses.color_pair(2))
                 self.stdscr.attron(curses.A_BOLD)
-                self.stdscr.addstr(1 + i, 20, migration.stringify() + ' (curr)', curses.color_pair(2))
+                self.stdscr.addstr(1 + i, 20,
+                                   migration.stringify() + ' (curr)',
+                                   curses.color_pair(2))
                 self.stdscr.attroff(curses.color_pair(2))
                 self.stdscr.attroff(curses.A_BOLD)
             else:
-                self.stdscr.addstr(1 + i, 20, migration.stringify(), curses.color_pair(1))
-            
+                self.stdscr.addstr(1 + i, 20, migration.stringify(),
+                                   curses.color_pair(1))
 
     def render_alembic_install_error(self):
         self.render_error("Alembic is not Installed")
@@ -129,11 +159,13 @@ class Atig():
 
     def render_error(self, message):
         # Declaration of strings
-        title = message[:self.width-1]
-        keystr = "Press q to exit"[:self.width-1]
+        title = message[:self.width - 1]
+        keystr = "Press q to exit"[:self.width - 1]
         # Centering calculations
-        start_x_title = int((self.width // 2) - (len(title) // 2) - len(title) % 2)
-        start_x_keystr = int((self.width // 2) - (len(keystr) // 2) - len(keystr) % 2)
+        start_x_title = int((self.width // 2) - (len(title) // 2) -
+                            len(title) % 2)
+        start_x_keystr = int((self.width // 2) - (len(keystr) // 2) -
+                             len(keystr) % 2)
         start_y = int((self.height // 2) - 2)
 
         # Turning on attributes for title
@@ -150,7 +182,7 @@ class Atig():
         # Print rest of text
         self.stdscr.addstr(start_y + 3, (self.width // 2) - 2, '-' * 4)
         self.stdscr.addstr(start_y + 5, start_x_keystr, keystr)
-    
+
     def is_in_alembic_directory(self):
         return os.path.exists('alembic') and \
            os.path.exists('alembic/versions')
@@ -159,12 +191,10 @@ class Atig():
         return os.popen('which alembic').read() != ''
 
 
-
-
-
 def main():
     atig = Atig()
     curses.wrapper(atig.init_screen)
+
 
 if __name__ == "__main__":
     main()
--- atig/migration.py
+++ atig/migration.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Migration():
     def __init__(self, path):
         self.path = path
@@ -20,30 +21,32 @@ class Migration():
     def stringify(self):
         return f'{self.hash} - {self.desc}'
 
+
 class MigrationCollection():
     def __init__(self, migrations):
         # take in a list of migrations, build a hashmap
         self.migrations = {m.hash: m for m in migrations}
         self.history = {m.prev: m.hash for m in migrations}
         self.initial = list(filter(lambda x: 'None' in x.prev, migrations))[0]
-        self.current = os.popen('alembic current').read().split('\n')[-2].split(' ')[0]
-        self.heads = [head.split(' ')[0] for head in os.popen('alembic heads').read().split('\n')]
-        #for head in self.heads:
-        #    curr = self.migrations.get(head)
-        #    curr.is_head = True
-
+        self.current = os.popen('alembic current').read().split(
+            '\n')[-2].split(' ')[0]
+        self.heads = [
+            head.split(' ')[0]
+            for head in os.popen('alembic heads').read().split('\n')
+            if head != ''
+        ]
+        for head in self.heads:
+            curr = self.migrations[head]
+            curr.is_head = True
+        # make list of migrations
 
     def list_migrations(self):
         # start with initial, loop over others
         curr = self.initial
+        stack = []
         while curr is not None:
-            yield curr
+            stack.append(curr)
             next_hash = self.history.get(curr.hash)
             curr = self.migrations.get(next_hash)
-
-
-
-    
-
-
-
+        while len(stack) > 0:
+            yield stack.pop()
